f5d17c3445f390fa4293412784659c4208fe2bbf
Richard Shackleton <richardshackleton@netconstruct.co.uk>
2019-02-17 17:33:35 +0000

    Remove ipv6

--- cmd/main.go
+++ cmd/main.go
@@ -24,6 +24,8 @@ var (
 func main() {
 	flag.Parse()
 
+	log.Println("Execute")
+
 	if *flgAccessTokenFile != "" {
 		f, err := ioutil.ReadFile(*flgAccessTokenFile)
 		if err != nil {
@@ -37,6 +39,7 @@ func main() {
 		fmt.Println("You are required to set the domain, host and access_token. Refer to './netlify-dynamic-dns -help' for more information!")
 		return
 	}
+
 	if *flgInterval == 5 && os.Getenv("INTERVAL") != "" {
 		i, err := strconv.Atoi(os.Getenv("INTERVAL"))
 		if err != nil {
@@ -53,11 +56,11 @@ func main() {
 }
 
 func doDNSUpdate() {
-	ipv4, ipv6, err := getPublicIP()
+	ipv4, err := getPublicIP()
 	if err != nil {
 		log.Fatalln(err, "Error Getting The Clients Public IP Address")
 	}
-	log.Println("Retrieved Your Public Ip's -", "IPv4:", ipv4, "IPv6:", ipv6)
+	log.Println("Retrieved Your Public Ip's -", "IPv4:", ipv4)
 
 	dnsRecords, err := getDNSRecords(*flgDomain, *flgAccessToken)
 	if err != nil {
@@ -65,7 +68,6 @@ func doDNSUpdate() {
 	}
 
 	ipv4Correct := false
-	ipv6Correct := false
 
 	for _, r := range dnsRecords {
 		if r.Hostname == *flgHost+"."+*flgDomain {
@@ -76,18 +78,11 @@ func doDNSUpdate() {
 					log.Println("Deleting The Incorrect or Duplicate IPv4 DNS Record!", r)
 					deleteDNSRecord(*flgDomain, *flgAccessToken, r.ID)
 				}
-			} else if r.Type == "AAAA" {
-				if r.Value == ipv6 && !ipv6Correct {
-					ipv6Correct = true
-				} else {
-					log.Println("Deleting The Incorrect or Duplicate IPv6 DNS Record!", r)
-					deleteDNSRecord(*flgDomain, *flgAccessToken, r.ID)
-				}
 			}
 		}
 	}
 
-	if ipv4Correct && ipv6Correct {
+	if ipv4Correct {
 		log.Println("DNS Records Are Correct. No Changes Are Being Made.")
 		return
 	}
@@ -99,19 +94,6 @@ func doDNSUpdate() {
 			Type:     "A",
 			Hostname: *flgHost,
 			Value:    ipv4,
-		})
-		if err != nil {
-			log.Fatalln(err, "Error Updating The Netlify DNS Entry")
-		}
-	}
-
-	if !ipv6Correct {
-		log.Println("Updating The IPv6 DNS Record!")
-
-		err = updateDNSRecord(*flgDomain, *flgAccessToken, netlifyDNSRecord{
-			Type:     "AAAA",
-			Hostname: *flgHost,
-			Value:    ipv6,
 		})
 		if err != nil {
 			log.Fatalln(err, "Error Updating The Netlify DNS Entry")
